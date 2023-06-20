d2ed6131bba7a719fbf2faa1141c23dc10d4b2a0
Jose Sanchez <engineer@copydataai.com>
2023-06-20 22:47:26 +0100

    Add(feat): Add ApplyTheme & BackupTheme with date

--- internal/themes/theme.go
+++ internal/themes/theme.go
@@ -1,7 +1,9 @@
 package themes
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	cp "github.com/otiai10/copy"
 )
@@ -22,4 +24,23 @@ func CreateThemes(configDirectory, themesDirectory string) error {
 	}
 
 	return nil
+}
+
+func ApplyTheme(pathTheme, alacrittyConfDir string) error {
+	err := cp.Copy(pathTheme, alacrittyConfDir)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func BackUpTheme(alacrittyConfDir string) (string, error) {
+	year, month, day := time.Now().Date()
+	backupPath := fmt.Sprintf("%s.%d%d%d.bak", alacrittyConfDir, year, month, day)
+	err := os.Rename(alacrittyConfDir, backupPath)
+	if err != nil {
+		return "", err
+	}
+
+	return backupPath, nil
 }
